de3d35d9bfc89aa6eefc911e7543e11896fa57ff
takeher <2582253630@qq.com>
2021-01-20 20:13:59 +0800

    upload test

--- listclass/list.cpp
+++ listclass/list.cpp
@@ -9,7 +9,7 @@ class list_t
 		double data;
 	};
 public:
-	list_t (){(*node).next = node;(*node).prev = node;}
+	list_t (){(*node).next = node;(*node).prev = node;length=0;}
 	list_t (const list_t& x);
 	virtual ~list_t ();
 	class iterator;
@@ -18,14 +18,14 @@ public:
 	void pop_front();
 	void pop_back();
 	iterator insert(iterator pos, const double& x);
-	void erase(iterator pos, const double& x);
+	void erase(iterator pos);
 	void erase(iterator first, iterator last);
 	iterator begin();
 	iterator end();
 	void splice(iterator pos, list_t& x);
 	unsigned size() const;
 	bool empty() const;
-	void sort();
+	//void sort();
 	list_t& operator=(const list_t& x);
 
 protected:
@@ -37,14 +37,14 @@ class list_t::iterator
 {
 public:
 	iterator(){}
-	iterator(node_d* x):node_t(x){}
+	//iterator(node_d* x):node(x){}
 	double operator*() const
 	{
-		return (*node_t).data;
+		return (*node).data;
 	}
 	iterator& operator++()
 	{
-		node_t = (*node_t).next;
+		node = (*node).next;
 		return *this;
 	}
 	iterator operator++(int)
@@ -53,13 +53,115 @@ public:
 		++*this;
 		return tmp;
 	}
+	iterator& operator--()
+	{
+		node = (*node).prev;
+		return *this;
+	}
+	iterator operator--(int)
+	{
+		iterator tmp = *this;
+		--*this;
+		return tmp;
+	}
+
 	bool operator==(const iterator& x)
 	{
-		if(this == &x)
+		if(this->node == x.node)
 			return true;
 		else 
 			return false;
 	}
-protected:
-	node_d* node_t;
+	bool operator!=(const iterator& x)
+	{
+		if(x.node == this->node)
+			return false;
+		else 
+			return true;
+	}
+//protected:
+	node_d* node;
+	iterator(node_d*x):node(x){}
 };
+bool list_t::empty() const
+{
+	if((*node).next==node and (*node).prev==node)
+		return true;
+	else 
+		return false;
+}
+list_t::iterator list_t::begin()
+{
+	return (*node).next;
+}
+list_t::iterator list_t::end()
+{
+	return node;
+}
+list_t::iterator list_t::insert(iterator pos, const double &x)
+{
+	node_d* p = new node_d;
+	p->data = x;
+	p->next = pos.node;
+	p->prev = (*pos.node).prev;
+	(*(*pos.node).prev).next = p;
+	(*pos.node).prev = p;
+	++length;
+	return p;
+}
+void list_t::push_front(const double &x)
+{
+	insert(begin(), x);
+}
+void list_t::push_back(const double &x)
+{
+	insert(end(), x);
+}
+void list_t::erase(iterator pos)
+{
+	node_d* p = new node_d;
+	p = pos.node;
+	(p->prev)->next = p->next;
+	(p->next)->prev = p->prev;
+	delete p;
+	--length;
+}
+void list_t::erase(iterator first,iterator last)
+{
+	for(iterator itr = first;itr != last;++itr)
+	{	
+		erase(itr);
+		--length;
+	}
+}
+void list_t::pop_front()
+{	
+	if(!empty())
+		erase(begin());
+	else 
+		cout<<"nothing to pop"<<endl;
+}
+void list_t::pop_back()
+{
+	if(!empty())
+		erase(--end());
+	else 
+		cout<<"nothing to pop"<<endl;
+}
+unsigned list_t::size() const
+{
+	return length;
+}
+void list_t::splice(iterator pos, list_t &x)
+{
+	for(auto itr = x.begin();itr != x.end();++itr)
+	{	
+		insert(++pos,*itr);
+		++length;
+	}
+}
+list_t& list_t::operator=(const list_t& x)
+{
+	this->node = x.node;
+	return *this;
+}
