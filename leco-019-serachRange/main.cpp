a64eb3bef7a826403036050e0e74952c76009e19
takeher <2582253630@qq.com>
2021-02-08 10:05:10 +0800

    upload test

---
+++ leco-019-serachRange/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target, bool lower) {
+        int left = 0, right = (int)nums.size() - 1, ans = (int)nums.size();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.size() && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return vector<int>{leftIdx, rightIdx};
+        }
+        return vector<int>{-1, -1};
+    }
+};
+int main(int argc, char *argv[])
+{
+	vector<int> nums{5,7,7,8,8,10};
+	int target = 8;
+	Solution sol;
+	vector<int> ans = sol.searchRange(nums, target);
+	cout<<"ans is:["<<ans[0]<<","<<ans[1]<<"]"<<endl;
+	return 0;
+}
