50dcf3e3834d2266a50778aff06c059e71bcd173
takeher <2582253630@qq.com>
2021-02-11 09:34:07 +0800

    upload test

---
+++ leco-021-combineSum/main2.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+private:
+    vector<pair<int, int>> freq;
+    vector<vector<int>> ans;
+    vector<int> sequence;
+
+public:
+    void dfs(int pos, int rest) {
+        if (rest == 0) {
+            ans.push_back(sequence);
+            return;
+        }
+        if (pos == freq.size() || rest < freq[pos].first) {
+            return;
+        }
+
+        dfs(pos + 1, rest);
+
+        int most = min(rest / freq[pos].first, freq[pos].second);
+        for (int i = 1; i <= most; ++i) {
+            sequence.push_back(freq[pos].first);
+            dfs(pos + 1, rest - i * freq[pos].first);
+        }
+        for (int i = 1; i <= most; ++i) {
+            sequence.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        for (int num: candidates) {
+            if (freq.empty() || num != freq.back().first) {
+                freq.emplace_back(num, 1);
+            } else {
+                ++freq.back().second;
+            }
+        }
+        dfs(0, target);
+        return ans;
+    }
+};
+int main(int argc, char *argv[])
+{
+	vector<int> nums{10,1,2,7,6,1,5};
+	int target = 8;
+	Solution sol;
+	vector<vector<int>> ans = sol.combinationSum2(nums, target);
+	for(int i = 0;i < nums.size();++i){
+		for(int j = 0;j < ans[i].size();++j){
+			cout<<ans[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
