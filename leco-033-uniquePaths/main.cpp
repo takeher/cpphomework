ace47396f4f5b2f0755506853104185b1ce043aa
takeher <2582253630@qq.com>
2021-02-23 09:48:26 +0800

    upload test

---
+++ leco-033-uniquePaths/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return ans;
+    }
+};
+int main(int argc, char *argv[])
+{
+	int m = 7;
+	int n = 3;
+	Solution sol;
+	int ans = sol.uniquePaths(m, n);
+	std::cout<<"ans is:"<<ans<<std::endl;
+	return 0;
+}
+/*
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> f(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+};
+*/
