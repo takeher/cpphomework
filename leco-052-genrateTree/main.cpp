2953b33a8db6612212b819bbfff3cad3cd0e9afd
takeher <2582253630@qq.com>
2021-03-15 09:05:14 +0800

    upload test

---
+++ leco-052-genrateTree/main.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct TreeNode {
+	int val;
+     	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int start, int end) {
+        if (start > end) {
+            return { nullptr };
+        }
+        vector<TreeNode*> allTrees;
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            vector<TreeNode*> leftTrees = generateTrees(start, i - 1);
+
+            // 获得所有可行的右子树集合
+            vector<TreeNode*> rightTrees = generateTrees(i + 1, end);
+
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (auto& left : leftTrees) {
+                for (auto& right : rightTrees) {
+                    TreeNode* currTree = new TreeNode(i);
+                    currTree->left = left;
+                    currTree->right = right;
+                    allTrees.emplace_back(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (!n) {
+            return {};
+        }
+        return generateTrees(1, n);
+    }
+};
+class inorder {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode *predecessor = nullptr;
+
+        while (root != nullptr) {
+            if (root->left != nullptr) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root->left;
+                while (predecessor->right != nullptr && predecessor->right != root) {
+                    predecessor = predecessor->right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor->right == nullptr) {
+                    predecessor->right = root;
+                    root = root->left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.push_back(root->val);
+                    predecessor->right = nullptr;
+                    root = root->right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	int n = 3;
+	Solution sol;
+	inorder order;
+	auto ans = sol.generateTrees(n);
+	cout<<"ans is:"<<endl;
+	vector<vector<int>> tree;
+	for(int i = 0;i < ans.size();++i){
+		tree.push_back(order.inorderTraversal(ans[i]));
+	}
+	for(int i = 0;i < tree.size();++i){
+		for(int j = 0;j < tree[i].size();++j){
+			cout<<tree[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
