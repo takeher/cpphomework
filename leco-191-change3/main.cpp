492b0a8cbe1ba10f8f8faa04c59e29d4e6b96017
takeher <2582253630@qq.com>
2021-08-02 09:01:48 +0800

    upload 20210802

---
+++ leco-191-change3/main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (int& coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+int main(int argc, char *argv[])
+{
+	vector<int> coins {1,2,5};
+	Solution sol;
+	cout<<"ans is:"<<sol.change(5, coins)<<endl;
+	return 0;
+}
