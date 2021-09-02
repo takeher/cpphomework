11d81d2c6d2a9fa50b48a97666dafa369e5d6235
takeher <2582253630@qq.com>
2021-09-02 12:18:32 +0800

    upload 20210902

---
+++ leco-222-judgeSquareSum/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  bool judgeSquareSum(int c) {
+    for (int base = 2; base * base <= c; base++) {
+      // 如果不是因子，枚举下一个
+      if (c % base != 0) {
+        continue;
+      }
+
+      // 计算 base 的幂
+      int exp = 0;
+      while (c % base == 0) {
+        c /= base;
+        exp++;
+      }
+
+      // 根据 Sum of two squares theorem 验证
+      if (base % 4 == 3 && exp % 2 != 0) {
+        return false;
+      }
+    }
+
+    // 例如 11 这样的用例，由于上面的 for 循环里 base * base <= c ，base == 11
+    // 的时候不会进入循环体 因此在退出循环以后需要再做一次判断
+    return c % 4 != 3;
+  }
+};
+int main(){
+  cout<<(new Solution())->judgeSquareSum(25)<<endl;
+  return 0;
+}
