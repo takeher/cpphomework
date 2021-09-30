2917b6567e7ec7ad2f27f82783c4cb4cee6cc1e9
takeher <2582253630@qq.com>
2021-09-30 10:42:35 +0800

    upload 20210930

---
+++ .ccls-cache/@Users@car1999@code@cpp@cpphomework/leco-250-findLength@main.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    const int mod = 1000000009;
+    const int base = 113;
+    // 使用快速幂计算 x^n % mod 的值
+    long long qPow(long long x, long long n) {
+        long long ret = 1;
+        while (n) {
+            if (n & 1) {
+                ret = ret * x % mod;
+            }
+            x = x * x % mod;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    bool check(const vector<int>& A, const vector<int>& B, int len) {
+        long long hashA = 0;
+        for (int i = 0; i < len; i++) {
+            hashA = (hashA * base + A[i]) % mod;
+        }
+        unordered_set<long long> bucketA;
+        bucketA.insert(hashA);
+        long long mult = qPow(base, len - 1);
+        for (int i = len; i < A.size(); i++) {
+            hashA = ((hashA - A[i - len] * mult % mod + mod) % mod * base + A[i]) % mod;
+            bucketA.insert(hashA);
+        }
+        long long hashB = 0;
+        for (int i = 0; i < len; i++) {
+            hashB = (hashB * base + B[i]) % mod;
+        }
+        if (bucketA.count(hashB)) {
+            return true;
+        }
+        for (int i = len; i < B.size(); i++) {
+            hashB = ((hashB - B[i - len] * mult % mod + mod) % mod * base + B[i]) % mod;
+            if (bucketA.count(hashB)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int findLength(const vector<int>& A, const vector<int>& B) {
+        int left = 1, right = min(A.size(), B.size()) + 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (check(A, B, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+};
+int main(){
+  cout << "ans is:"
+       << (new Solution())
+              ->findLength(vector<int>{1, 2, 3, 2, 1},
+                           vector<int>{3, 2, 1, 4, 7})<<endl;
+  return 0;
+}
