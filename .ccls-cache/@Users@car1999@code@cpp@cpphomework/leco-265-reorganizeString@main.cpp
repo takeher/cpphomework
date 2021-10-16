00d4a0ab644a29fa16d3e44bfe15b71a4ed53de3
takeher <2582253630@qq.com>
2021-10-16 13:32:32 +0800

    upload 20211016

---
+++ .ccls-cache/@Users@car1999@code@cpp@cpphomework/leco-265-reorganizeString@main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        vector<int> counts(26, 0);
+        int maxCount = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s[i];
+            counts[c - 'a']++;
+            maxCount = max(maxCount, counts[c - 'a']);
+        }
+        if (maxCount > (length + 1) / 2) {
+            return "";
+        }
+        string reorganizeArray(length, ' ');
+        int evenIndex = 0, oddIndex = 1;
+        int halfLength = length / 2;
+        for (int i = 0; i < 26; i++) {
+            char c = 'a' + i;
+            while (counts[i] > 0 && counts[i] <= halfLength && oddIndex < length) {
+                reorganizeArray[oddIndex] = c;
+                counts[i]--;
+                oddIndex += 2;
+            }
+            while (counts[i] > 0) {
+                reorganizeArray[evenIndex] = c;
+                counts[i]--;
+                evenIndex += 2;
+            }
+        }
+        return reorganizeArray;
+    }
+};
+int main(){
+  cout<<"ans is:"<<(new Solution())->reorganizeString("aab")<<endl;
+  return 0;
+}
