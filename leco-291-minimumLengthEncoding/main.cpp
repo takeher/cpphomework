93db1e17e9c2ffa2f093875012f2051997b0c90f
takeher <2582253630@qq.com>
2021-11-11 00:10:27 +0800

    upload 20211111

---
+++ leco-291-minimumLengthEncoding/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode{
+    TrieNode* children[26];
+public:
+    int count;
+    TrieNode() {
+        for (int i = 0; i < 26; ++i) children[i] = NULL;
+        count = 0;
+    }
+    TrieNode* get(char c) {
+        if (children[c - 'a'] == NULL) {
+            children[c - 'a'] = new TrieNode();
+            count++;
+        }
+        return children[c - 'a'];
+    }
+};
+class Solution {
+public:
+    int minimumLengthEncoding(const vector<string>& words) {
+        TrieNode* trie = new TrieNode();
+        unordered_map<TrieNode*, int> nodes;
+
+        for (int i = 0; i < (int)words.size(); ++i) {
+            string word = words[i];
+            TrieNode* cur = trie;
+            for (int j = word.length() - 1; j >= 0; --j)
+                cur = cur->get(word[j]);
+            nodes[cur] = i;
+        }
+
+        int ans = 0;
+        for (auto& [node, idx] : nodes) {
+            if (node->count == 0) {
+                ans += words[idx].length() + 1;
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+  cout << "ans is:"<< (new Solution())->minimumLengthEncoding(vector<string>{"time", "me", "bell"})<<endl;
+  return 0;
+}
