4f2b3751418432860495fd2796a7ab7078a6ef22
takeher <2582253630@qq.com>
2021-12-08 12:14:28 +0800

    upload 20211208

---
+++ .ccls-cache/@Users@car1999@code@cpp@cpphomework/leco-318-stoneGame@main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool stoneGame(vector<int> &piles) { return true; }
+};
+int main(){
+  cout<<"ans is:"<<true<<endl;
+  return 0;
+}
